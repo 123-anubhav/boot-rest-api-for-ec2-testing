288241dec43d04c5bd91a4fb0359c65f4539377c
123-anubhav <anubhav.aa2@gmail.com>
2025-07-15 21:50:30 +0530

    logging added using @slf4j

--- Ec2TestRest/src/main/java/in/anubhav/rest/RestControllerForEc2.java
+++ Ec2TestRest/src/main/java/in/anubhav/rest/RestControllerForEc2.java
@@ -1,18 +1,31 @@
 package in.anubhav.rest;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.anubhav.dto.Student;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class RestControllerForEc2 {
+
+	private static final Logger logger = LoggerFactory.getLogger(RestControllerForEc2.class);
 
 	@GetMapping("/students")
 	public List<Student> listOfStu(){
+		
+	   logger.info("execution start at in.anubhav.rest.RestControllerForEc2.listOfStu()");
+
 		Student s1=new Student(101,"abhay");
 		Student s2=new Student(102,"ranjan");
 		Student s3=new Student(103,"vivek");
@@ -22,8 +35,8 @@ public class RestControllerForEc2 {
 		li.add(s2);
 		li.add(s3);
 		
-		System.out.println(li);
-		
+		logger.info("Returning result: { list of student is }"+ li);
+		System.out.println(li);		
 		return li;
 		
 	}
